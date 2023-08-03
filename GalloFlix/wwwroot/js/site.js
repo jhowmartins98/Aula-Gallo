231ed3af25541fa6177b3a710d4ffe79f1f0d611
jhowmartins98 <jhowhmartins@outlook.com>
2023-08-02 22:16:33 -0300

    refazer o banco

---
+++ GalloFlix/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿function showLoading() {
+    const loader = document.querySelector('.loader');
+    if (loader != null) {
+        loader.classList.remove('d-none');
+    }
+};
+
+function hideLoading() {
+    const loader = document.querySelector('.loader');
+    if (loader != null) {
+        loader.classList.add('d-none');
+    }
+};
+
+$(document).ready(function () {
+    hideLoading();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.querySelector('form');
+    console.log(form);
+    if (form != undefined){ 
+        form.addEventListener('submit', function() {
+            if ($(form).valid() === true) 
+                showLoading();
+        })
+    }
+});
